e51c32750d7f2a2981ec2dc67784f4f4fe587768
David Garcia Perez <davidgape89@gmail.com>
2016-09-13 21:18:04 +0200

    Changed seed

--- karma.conf.js
+++ karma.conf.js
@@ -1,57 +1,132 @@
-﻿module.exports = function (config) {
-	config.set({
-		// base path, that will be used to resolve files and exclude
-		basePath: '',
+// Karma configuration
+// Generated on Wed Jul 15 2015 09:44:02 GMT+0200 (Romance Daylight Time)
+'use strict';
 
-		// frameworks to use
-		frameworks: ['mocha', 'chai', 'chai-sinon'],
+var argv = require('yargs').argv;
 
-		files: [],
+module.exports = function (config) {
+  config.set({
 
-		// list of files to exclude
-		exclude: [],
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: './',
 
-		// test results reporter to use
-		// possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-		reporters: ['dots'],
 
-		// web server port
-		port: 9876,
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
 
-		// enable / disable colors in the output (reporters and logs)
-		colors: true,
 
-		// level of logging
-		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-		logLevel: config.LOG_INFO,
+    // list of files / patterns to load in the browser
+    files: [
+      // Polyfills.
+      'node_modules/core-js/client/shim.min.js',
 
-		// enable / disable watching file and executing tests whenever any file changes
-		autoWatch: false,
+      'node_modules/traceur/bin/traceur.js',
 
-		// Start these browsers, currently available:
-		// - Chrome
-		// - ChromeCanary
-		// - Firefox
-		// - Opera
-		// - Safari (only Mac)
-		// - PhantomJS
-		// - IE (only Windows)
-		browsers: ['Chrome', 'Firefox', 'IE'],
+      // System.js for module loading
+      'node_modules/systemjs/dist/system.src.js',
 
-		// If browser does not capture in given timeout [ms], kill it
-		captureTimeout: 60000,
+      // Zone.js dependencies
+      'node_modules/zone.js/dist/zone.js',
+      'node_modules/zone.js/dist/long-stack-trace-zone.js',
+      'node_modules/zone.js/dist/async-test.js',
+      'node_modules/zone.js/dist/fake-async-test.js',
+      'node_modules/zone.js/dist/sync-test.js',
+      'node_modules/zone.js/dist/proxy.js',
+      'node_modules/zone.js/dist/jasmine-patch.js',
 
-		// Continuous Integration mode
-		// if true, it capture browsers, run tests and exit
-		singleRun: true,
+      // RxJs.
+      { pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false },
+      { pattern: 'node_modules/rxjs/**/*.js.map', included: false, watched: false },
 
-		plugins: [
-			'karma-mocha',
-			'karma-chai',
-			'karma-chai-sinon',
-			'karma-chrome-launcher',
-			'karma-firefox-launcher',
-			'karma-ie-launcher'
-		]
-	});
+      // paths loaded via module imports
+      // Angular itself
+      { pattern: 'node_modules/@angular/**/*.js', included: false, watched: true },
+      { pattern: 'node_modules/@angular/**/*.js.map', included: false, watched: false },
+
+      { pattern: 'dist/dev/**/*.js', included: false, watched: true },
+      { pattern: 'dist/dev/**/*.html', included: false, watched: true, served: true },
+      { pattern: 'dist/dev/**/*.css', included: false, watched: true, served: true },
+      { pattern: 'node_modules/systemjs/dist/system-polyfills.js', included: false, watched: false }, // PhantomJS2 (and possibly others) might require it
+
+      // suppress annoying 404 warnings for resources, images, etc.
+      { pattern: 'dist/dev/assets/**/*', watched: false, included: false, served: true },
+
+      'test-config.js',
+      'dist/dev/app/system-config.js',
+      'test-main.js'
+    ],
+
+    // must go along with above, suppress annoying 404 warnings.
+    proxies: {
+      '/assets/': '/base/dist/dev/assets/'
+    },
+
+    // list of files to exclude
+    exclude: [
+      'node_modules/**/*spec.js'
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['mocha'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: [
+      'Chrome'
+    ],
+
+
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Passing command line arguments to tests
+    client: {
+      files: argv.files
+    }
+  });
+
+  if (process.env.APPVEYOR) {
+    config.browsers = ['IE'];
+    config.singleRun = true;
+    config.browserNoActivityTimeout = 90000; // Note: default value (10000) is not enough
+  }
+
+  if (process.env.TRAVIS || process.env.CIRCLECI) {
+    config.browsers = ['Chrome_travis_ci'];
+    config.singleRun = true;
+    config.browserNoActivityTimeout = 90000;
+  }
 };
